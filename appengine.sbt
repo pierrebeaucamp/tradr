a9382767244668a045bd1f34502fff851adbb0ed
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-06-07 22:32:51 +0200

    Fixed some bugs in Davids code

--- appengine.sbt
+++ appengine.sbt
@@ -2,7 +2,7 @@ import sbtappengine.Plugin.{AppengineKeys => gae}
 import play.PlayProject
 
 name := "PlayFramework-AppEngine"
-version := "0.0.2"
+version := "0.0.3"
 scalaVersion := "2.10.2"
 
 resolvers += "Scala AppEngine Sbt Repo" at "http://siderakis.github.com/maven"
--- src/main/java/com/appspot/tradr_seba/Application.java
+++ src/main/java/com/appspot/tradr_seba/Application.java
@@ -2,17 +2,15 @@ package com.appspot.tradr_seba;
 
 import com.google.appengine.api.blobstore.*;
 import com.google.appengine.api.datastore.*;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.images.*;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
-import twirl.api.Html;
 import scala.collection.immutable.*;
 import scala.collection.JavaConverters.*;
+import twirl.api.Html;
+
 
 public class Application {
     
@@ -21,30 +19,18 @@ public class Application {
     private static ImagesService images = ImagesServiceFactory.getImagesService();
 
     public static String index() {
-
-	//Filter allTheTitles = new FilterPredicate("title",FilterOperator.EQUAL,"stupid");
-	//Query query = new Query("title").setFilter(allTheTitles);
-	Query query = new Query("title");
-	//query.addSort("date_entered",Query.SortDirection.ASCENDING);
-	//FetchOptions options = FetchOptions.Builder.withLimit(25);
-
-	//List<com.google.appengine.api.datastore.Entity> entities = datastore.prepare(query).asList(options);
-	//scala.collection.immutable.List<com.google.appengine.api.datastore.Entity> items = scalaList(entities);
+        Query query = new Query("Item");
         
-	// Use PreparedQuery interface to retrieve results
-	PreparedQuery pq = datastore.prepare(query);
-
-	String title = "";
-	int num = pq.countEntities();
-	for (Entity result : pq.asIterable()) {
-  		title = (String) result.getProperty("title");
-		System.out.println("Entrada con titulo"+title);
-	}
-	return html.index.render(num).toString();
-        //return html.index.render().toString();
+        //query.addSort("date_entered",Query.SortDirection.ASCENDING);
+        
+        FetchOptions options = FetchOptions.Builder.withLimit(25);
+        List<com.google.appengine.api.datastore.Entity> entities = datastore.prepare(query).asList(options);
+        
+        scala.collection.immutable.List<com.google.appengine.api.datastore.Entity> items = scalaList(entities);
+        return html.index.render(items).toString();
     }
 
-    public static <T> scala.collection.immutable.List<T> scalaList(List<T> javaList) {
+    private static <T> scala.collection.immutable.List<T> scalaList(List<T> javaList) {
         return scala.collection.JavaConversions.asScalaIterable(javaList).toList();
     }
 
