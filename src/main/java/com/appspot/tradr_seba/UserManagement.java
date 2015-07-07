17d20b9782333ca79bdfcc67d76d8ac20ed0c0ef
Feng <GabrielSeifert@sgnbtest02.speedport_w_921v_1_37_000>
2015-07-07 14:31:45 +0200

    saved username in a session after user login

--- src/main/java/com/appspot/tradr_seba/UserManagement.java
+++ src/main/java/com/appspot/tradr_seba/UserManagement.java
@@ -7,6 +7,11 @@ import com.google.appengine.api.images.*;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.naming.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -20,20 +25,30 @@ public class UserManagement {
     private static BlobstoreService blobstore = BlobstoreServiceFactory.getBlobstoreService();
     private static DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     private static ImagesService images = ImagesServiceFactory.getImagesService();
-
-    public static String index(){
-    	//HttpSession session =request.getSession();
-    	//String UN = session.getAttribute("username").toString();
-    	return html.register.render("","","").toString();
+	
+	
+    public static String index(HttpServletRequest request){
+    	HttpSession session =request.getSession();
+    	String username = session.getAttribute("username").toString();
+    	return html.register.render(username,"","","","").toString();
     }
-    public static String change(){
-    	return html.changepassword.render("initail").toString();
+    public static String change(HttpServletRequest request){
+    	HttpSession session =request.getSession();
+    	String username = session.getAttribute("username").toString();
+    	return html.changepassword.render(username,"initail").toString();
     }
-    public static String findpasswordback(){
-    	return html.findpassword.render().toString();
+    public static String findpasswordback(HttpServletRequest request){
+    	HttpSession session =request.getSession();
+    	String username = session.getAttribute("username").toString();
+    	return html.findpassword.render(username).toString();
     }
-    public static String user(){
-    	return html.user.render("not").toString();
+    public static String user(HttpServletRequest request) {
+    	HttpSession session = request.getSession();
+    	String username = session.getAttribute("username").toString();
+    	if (username !=null)
+    		return html.user.render("yes",username).toString();
+    	else
+    	    return html.user.render("not","").toString();
     }
 
     public static String getUser(HttpServletRequest request) throws EntityNotFoundException {
@@ -44,27 +59,33 @@ public class UserManagement {
 	String loginpassword = request.getParameter("LoginPassword").toString();
 	Entity user = null;
     try {user = datastore.get(KeyFactory.createKey("User", loginusername));}
-    catch (EntityNotFoundException e) {return html.user.render("usernot").toString();}
+    catch (EntityNotFoundException e) {return html.user.render("usernot","").toString();}
 
     String username = user.getProperty("username").toString();
     String password = user.getProperty("password").toString();
     
     if(loginpassword.equals(password)){
-    	//session.setAttribute("username",username);
-        return html.user.render("yes").toString();
+    	HttpSession session = request.getSession();
+    	session.setAttribute("username",username);
+        return html.user.render("yes",username).toString();
     }
 
     else {
-    	return html.user.render("passwordnot").toString();
+    	return html.user.render("passwordnot","").toString();
     }
        
 }
+    public static String Logout(HttpServletRequest request){
+    	HttpSession session = request.getSession();
+    	session.removeAttribute("username");
+    	return html.user.render("not","").toString();
+    }
 
     
     
-   public static String addUser(HttpServletRequest request) {
-	  //HttpSession session = request.getSession();
-	  //String UN = session.getAttribute("username").toString();
+   public static String addUser(HttpServletRequest request){
+   	HttpSession session =request.getSession();
+   	String US = session.getAttribute("username").toString();
 	   
         Entity user = new Entity("User",request.getParameter("username").toString());
 
@@ -76,12 +97,20 @@ public class UserManagement {
         String repeatpassword = request.getParameter("repeatpassword").toString();
         
         if (!username.substring(0,1).matches("[a-zA-Z]"))
-           return html.register.render("userfail","","").toString();
+           return html.register.render(US,"userfail","","","").toString();
         if (password.length()>12||password.length()<6)      	
-        	return html.register.render("","passwordfail","").toString();
+        	return html.register.render(US,"","passwordfail","","").toString();
         if( ! password.equals(repeatpassword))
-        	return html.register.render("","","passwordnotthesame").toString();
-                
+        	return html.register.render(US,"","","passwordnotthesame","").toString();
+         try {InternetAddress emailaddress = new InternetAddress(email);
+              emailaddress.validate();}
+         catch(AddressException ex){
+        	  return html.register.render(US,"","","","emailfail").toString();
+         }
+         
+     	Entity USER = null;
+        try {USER = datastore.get(KeyFactory.createKey("User", username));}
+         catch (EntityNotFoundException e){
         user.setProperty("username", username);
         user.setProperty("sex", request.getParameter("sex"));
         user.setProperty("date_entered", dateEntered);
@@ -95,6 +124,8 @@ public class UserManagement {
         String newaccount = "You have successfully created a new account!";
         String enjoy = "Enjoy your tradr!";
         return html.afterregister.render(welcome,newaccount,enjoy).toString(); 
+         }
+        return html.register.render(US,"existing","","","").toString();
     }
    
    public static String showuser () {
@@ -107,18 +138,20 @@ public class UserManagement {
    }
    
    public static String ChangePassword(HttpServletRequest request)  throws EntityNotFoundException {
+   	HttpSession session =request.getSession();
+   	String US = session.getAttribute("username").toString();
 	   	   
 	  String usernameinchange = request.getParameter("usernameinchange").toString();
 	  String oldpassword = request.getParameter("oldpassword").toString();
 	  String newpassword = request.getParameter("newpassword").toString();
 	  Entity user = null;
 	  try{ user = datastore.get(KeyFactory.createKey("User", usernameinchange));}
-	  catch (EntityNotFoundException e) { return html.changepassword.render("userfail").toString(); }
+	  catch (EntityNotFoundException e) { return html.changepassword.render(US,"userfail").toString(); }
 	  String username = user.getProperty("username").toString();
 	  String password = user.getProperty("password").toString();	
 
 		  if (password.equals(newpassword)){			  
-			  return html.changepassword.render("passwordfail").toString();
+			  return html.changepassword.render(US,"passwordfail").toString();
 		  }
 		  else{
 			  user.setProperty("password",newpassword);
@@ -128,6 +161,8 @@ public class UserManagement {
    }
    
    public static String FindPassword(HttpServletRequest request) throws EntityNotFoundException{
+   	HttpSession session =request.getSession();
+   	String US = session.getAttribute("username").toString();
 	   
 	   String usernameinback = request.getParameter("usernameinback").toString();
 	   String emailinback = request.getParameter("emailinback").toString();	   
